b4b970a580e4a2f44ef51898e458fd80454bc37c
MLDev33 <ypmarielaure@yahoo.fr>
2024-09-04 14:46:23 +0200

    fix route delete movielist

--- routes/movielists.js
+++ routes/movielists.js
@@ -78,18 +78,17 @@ router.get('/get/media/:id', async (req, res) => {
 
 
 /**
- * Route pour sauvegarder les paramètres d'une liste à sa création pour media type "Movies"
- * Nota l'ajout des media est traité par une fonction externe dasn un deuxième temps
- * avec la fonction addMediaMovie et addMediaTV
+ * Route pour sauvegarder les paramètres d'une liste à sa création
+ * Puis
  * 
  * @param { token }
  * 
  */
-router.post('/add/movie/:token', async (req, res) => {
+router.post('/add/:token', async (req, res) => {
 
     try {
         // Vérification de l'utilisateur
-        const user = await User.findOne({ token: req.params.token });
+        const user = await User.findOne({ token: req.params.token }); 
         if (!user) {
             return res.status(404).json({ success: false, message: "Utilisateur non trouvé" });
         }
@@ -98,14 +97,9 @@ router.post('/add/movie/:token', async (req, res) => {
         User.findOne({ token: req.params.token.toString() })
             .then(async data => {
                 if (data) {
-
-
-                    //const moviesListsMovie = await addMediaMovies(req.body, 1)
-                    //const moviesListsTV = await addMediaTv(req.body, 1)
-                    const mediaLists = await addMediaMovies(req.body, 1)
-
+                    const movieslists = await addMediaMovies(req.body, 1)
                     const newMovieLists = new MovieLists({
-                        token: data.token,
+                        token: data.token, // remplacer par id user
                         list_name: req.body.list_name,
                         creator: data._id,
                         options:
@@ -113,122 +107,28 @@ router.post('/add/movie/:token', async (req, res) => {
                             content_type: req.body.types,
                             genre: req.body.genres,
                             streaming_platform: req.body.providers,
-                            // average, releaseDateGte à rajouter au schema initial de movielists ?
+                            // average, releaseDateGte et ReleaseDateLte à rajouter au schema initial de movielists
                             average: req.body.rating,
                             releaseDateGte: req.body.releaseDateGte,
                             // releaseDateLte: req.body.releaseDateLte,
                         },
                         avatar: req.body.avatar,
-                        movies: mediaLists,
+                        movies: movieslists,
                     });
-                    console.log("moviesLists", mediaLists)
+                    console.log("moviesLists", movieslists)
                     newMovieLists.save().then(newDoc => {
                         user.created_list.push(newDoc._id);
                         user.save()
                         console.log("user created_list:", user)
                         console.log("type:", req.body.types)
-                        console.log("movies:", mediaLists)
-                        res.status(200).json({ result: true, result: newDoc })
+                        console.log("movies:", movieslists)
+                        res.status(200).json({ result: true, result: newDoc }) //movieLists
                     })
                 }
                 else {
                     res.status(400).json({ result: false, error: "User not found or name already exist" });
                 }
             });
-
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Erreur serveur", error: error.message });
-    }
-})
-
-/**
- * Route pour sauvegarder les paramètres d'une liste à sa création pour media type "TV"
- * Nota l'ajout des media est traité par une fonction externe dasn un deuxième temps
- * avec la fonction addMediaMovie et addMediaTV
- * 
- * @param { token }
- * 
- */
-router.post('/add/tv/:token', async (req, res) => {
-
-    try {
-        // Vérification de l'utilisateur
-        const user = await User.findOne({ token: req.params.token });
-        if (!user) {
-            return res.status(404).json({ success: false, message: "Utilisateur non trouvé" });
-        }
-
-        // crée une movieList en sauvegardant les paramètres de la liste
-        User.findOne({ token: req.params.token.toString() })
-            .then(async data => {
-                if (data) {
-
-
-                    //const moviesListsMovie = await addMediaMovies(req.body, 1)
-                    //const moviesListsTV = await addMediaTv(req.body, 1)
-                    const mediaLists = await addMediaTv(req.body, 1)
-
-                    const newMovieLists = new MovieLists({
-                        token: data.token,
-                        list_name: req.body.list_name,
-                        creator: data._id,
-                        options:
-                        {
-                            content_type: req.body.types,
-                            genre: req.body.genres,
-                            streaming_platform: req.body.providers,
-                            // average, releaseDateGte à rajouter au schema initial de movielists ?
-                            average: req.body.rating,
-                            releaseDateGte: req.body.releaseDateGte,
-                            // releaseDateLte: req.body.releaseDateLte,
-                        },
-                        avatar: req.body.avatar,
-                        movies: mediaLists,
-                    });
-                    console.log("moviesLists", mediaLists)
-                    newMovieLists.save().then(newDoc => {
-                        user.created_list.push(newDoc._id);
-                        user.save()
-                        console.log("user created_list:", user)
-                        console.log("type:", req.body.types)
-                        console.log("movies:", mediaLists)
-                        res.status(200).json({ result: true, result: newDoc })
-                    })
-                }
-                else {
-                    res.status(400).json({ result: false, error: "User not found or name already exist" });
-                }
-            });
-
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Erreur serveur", error: error.message });
-    }
-})
-
-
-// Route pour consulter les lists déjà existantes de l'utilisateur
-router.get('/user/lists/:token', async (req, res) => {
-
-
-    try {
-        const user = await User.findOne({ token: req.params.token }).populate('created_list');
-        console.log("user await:", user)
-        if (!user) {
-            return res.status(404).json({ success: false, message: "Utilisateur non trouvé" });
-        }
-
-        const listsMedia = user.created_list.map(list => ({
-            ...list.toObject(),
-        }));
-
-        console.log("lists:", listsMedia)
-
-        res.status(200).json({
-            success: true,
-            listsMedia,
-            totalList: listsMedia.length
-        });
-
 
     } catch (error) {
         res.status(500).json({ success: false, message: "Erreur serveur", error: error.message });
@@ -240,12 +140,12 @@ router.get('/user/lists/:token', async (req, res) => {
  * Fonction qui propose des medias à l'utilisateur selon les critères selectionnés
  * en paramètres dans l'application :
  * 1 - Name
- * 2 - Type: Movie ou TV
+ * 2 - Type: Movie ou serie
  * 3 - Genres
  * 4 - Minimum rating
  * 5 - Streaming platforms
  * 6 - Release date Gte
- * 
+ * 7 - Release date Lte
  * 
  * @param {(number|boolean|string|[]|{})} filters 
  * @returns un tableau d'objets contenant les media proposés à l'utilisateurs
@@ -267,9 +167,9 @@ async function addMediaMovies(filters, pagefilter) {
     } = filters
 
     let page = pagefilter;
-    let type = "movie"; // variable override => pb d'utilisation de Promise.all([ , ]) refaire deux...
+    let type = "movie"; // variable overrride => pb d'utilisation de Promise.all([ , ])
     genres = isEmpty(genres) ? "" : genres.map(genre => { return genre.id }).slice(",").join("|");
-    providers = isEmpty(providers) ? "" : providers.length === 1 ? providers[0].toString() : providers.slice(",").join("|");
+    providers = isEmpty(providers) ? "" : providers.lenght === 1 ? providers[0].toString() : providers.slice(",").join("|");
     average = isEmpty(average) ? "" : Number.parseInt(average);
 
 
@@ -284,7 +184,9 @@ async function addMediaMovies(filters, pagefilter) {
     let with_genres = isEmpty(genres) ? `` : `&with_genres=${genres}`;
     let with_watch_providers = isEmpty(providers) ? `` : `&with_watch_providers=${providers}`;
 
-    //Mémo url de base TMDB 
+    //console.log("req.body:", req.body)
+    //console.log("types", type)
+
     //const urlModulable = `https://api.themoviedb.org/3/discover/${types}?include_adult=${isAdult}&include_video=${isVideo}&language=${language}&page=${Number.parseInt(page)}&release_date.gte=${releaseDateGte.toString()}&release_date.lte=${releaseDateLte.toString()}&sort_by=${sortBy}&vote_average.gte=${average}&with_genres=${genres}&with_watch_providers=${providers}&api_key=${API_KEY}`;
     const urlModulable = `${BASE_URL}/discover/${type}?${include_adult}${include_video}${languageChoice}${pageSelect}${release_dateGte}${release_dateLte}${sort_by}${vote_averageGte}${with_genres}${with_watch_providers}&api_key=${API_KEY}`;
 
@@ -324,95 +226,7 @@ async function addMediaMovies(filters, pagefilter) {
     return listMedia
 }
 
-/**
- * Fonction qui propose des medias à l'utilisateur selon les critères selectionnés
- * en paramètres dans l'application :
- * 1 - Name
- * 2 - Type: Movie ou TV
- * 3 - Genres
- * 4 - Minimum rating
- * 5 - Streaming platforms
- * 6 - Release date Gte
- * 
- * 
- * @param {(number|boolean|string|[]|{})} filters 
- * @returns un tableau d'objets contenant les media proposés à l'utilisateurs
- */
-async function addMediaTv(filters, pagefilter) {
 
-
-    let {
-        types, // req.body.types "ATTENTION" Pour tuiliser la valeur mettre ne pas oublier le "s"
-        genres,
-        isAdult,
-        isVideo,
-        language,
-        releaseDateGte,
-        releaseDateLte,
-        sortBy,
-        average,
-        providers
-    } = filters
-
-    let page = pagefilter;
-    let type = "tv"; // variable override => pb d'utilisation de Promise.all([ , ]) refaire deux...
-    genres = isEmpty(genres) ? "" : genres.map(genre => { return genre.id }).slice(",").join("|");
-    providers = isEmpty(providers) ? "" : providers.length === 1 ? providers[0].toString() : providers.slice(",").join("|");
-    average = isEmpty(average) ? "" : Number.parseInt(average);
-
-
-    let include_adult = isEmpty(isAdult) ? `&include_adult=${false}` : `&include_adult=${isAdult}`;
-    let include_video = isEmpty(isVideo) ? `&include_video=${false}` : `&include_video=${isAdult}`;
-    let languageChoice = isEmpty(language) ? `&language=${"fr-Fr"}` : `&language=${language}`;
-    let pageSelect = isEmpty(page) ? `&page=${1}` : `&page=${Number.parseInt(page)}`;
-    let release_dateGte = isEmpty(releaseDateGte) ? `` : `&release_date.gte=${releaseDateGte.toString()}-01-01`;
-    let release_dateLte = isEmpty(releaseDateLte) ? `` : `&release_date.lte=${releaseDateLte.toString()}`;
-    let sort_by = isEmpty(sortBy) ? `&sort_by=${"popularity.desc"}` : `&sort_by=${sortBy}`;
-    let vote_averageGte = isEmpty(average) ? `` : `&vote_average.gte=${average}`;
-    let with_genres = isEmpty(genres) ? `` : `&with_genres=${genres}`;
-    let with_watch_providers = isEmpty(providers) ? `` : `&with_watch_providers=${providers}`;
-
-    //Mémo url de base TMDB 
-    //const urlModulable = `https://api.themoviedb.org/3/discover/${types}?include_adult=${isAdult}&include_video=${isVideo}&language=${language}&page=${Number.parseInt(page)}&release_date.gte=${releaseDateGte.toString()}&release_date.lte=${releaseDateLte.toString()}&sort_by=${sortBy}&vote_average.gte=${average}&with_genres=${genres}&with_watch_providers=${providers}&api_key=${API_KEY}`;
-    const urlModulable = `${BASE_URL}/discover/${type}?${include_adult}${include_video}${languageChoice}${pageSelect}${release_dateGte}${release_dateLte}${sort_by}${vote_averageGte}${with_genres}${with_watch_providers}&api_key=${API_KEY}`;
-
-    console.log("url", urlModulable)
-
-    const response = await fetch(urlModulable)
-    const data = await response.json()
-
-    console.log("DATA", data)
-
-    let contenu = [];
-
-    const listMediaTV = data.results.map((item) => {
-        //const movieId = item.id;
-        //const { listProviders, watchLink } = await getMoviesProviders(movieId, BASE_URL, API_KEY)
-        const releaseDateGte = moment(item.release_date || item.first_air_date).format('YYYY-MM-DD');
-
-        return {
-            type: item.type === 'movie' ? 'Film' : 'Série',
-            titre: item.title || item.name,
-            annee: releaseDateGte,
-            description: item.overview,
-            genre: item.genre_ids ? item.genre_ids.map(id => type === 'movie' ? MOVIE_GENRE_NAMES[id] : TV_GENRE_NAMES[id]).join(", ") : [],
-            poster: item.poster_path,
-            id: item.id,
-            popularite: item.vote_average,
-            vote: item.vote_count,
-            // plateformes: providers.map(p => ({
-            //     id: p.provider_id,
-            //     nom: p.provider_name,
-            //     logo: p.logo_path // Utilisez directement le logo_path du fournisseur
-            //   })),
-            // link: watchLink,
-        }
-    })
-    console.log("data:", data);
-    return listMediaTV
-}
-
-// In progress
 router.put('/moremedia/:id', async (req, res) => {
     try {
         const listMedia = await MovieLists.findById(req.params.id)
@@ -421,11 +235,11 @@ router.put('/moremedia/:id', async (req, res) => {
         }
 
         const moreMedia = await addMediaMovies(req.body, page++)
-
+        
 
         MovieLists.updateOne(
-            { movies: listMedia.movies },
-            { movie: [...listMedia.movies, moreMedia] }
+            {movies: listMedia.movies},
+            {movie: [...listMedia.movies, moreMedia]}
 
 
         ).then(() => {
@@ -445,8 +259,15 @@ router.put('/moremedia/:id', async (req, res) => {
 })
 
 
-router.delete("/delete/:id/:token", async (req, res) => {
 
+
+
+
+
+
+
+
+router.delete("/delete/:id/:token", async (req, res) => {
 
     try {
         // Vérification de l'utilisateur
@@ -455,54 +276,34 @@ router.delete("/delete/:id/:token", async (req, res) => {
             return res.status(404).json({ success: false, message: "Utilisateur non trouvé" });
         }
 
-        MovieLists.deleteOne({ _id: req.params.id }).then(() => {
-            MovieLists.find().then(data => {
-                console.log(data)
-            })
-        })
-        console.log(" result:", data.length)
-        res.status(200).json({ success: true, result: data })
-
-    } catch (error) {
-        console.error('Error occurred:', error);
-        res.status(500).json({ success: false, message: error.message });
-    }
-
-    
-
-});
-
-// Route à finir reste à récupérer l'id de la lste dans la population "created_list"
-// puis remettre la route en DELETE à la place de get 
-// ou transformer la route en fonction delete à ajouter à la route delete movieList
-router.get('/user/delete/list/:token/:id', async (req, res) => {
-
-    try {
-        const user = await User.findOne({ token: req.params.token.toString() }).populate("created_list");
-        console.log("user populate created_list:", user)
-        if (!user) {
-            return res.status(404).json({ success: false, message: "Utilisateur non trouvé" });
-        }
-        
-        
-        //res.status(200).json({ success: true, result: data })  
-                    
-
-
-
+        MovieLists.findOne({ _id: req.params.id }).then((data) => {
+            if (data) {
+                MovieLists.deleteOne({
+                    _id: req.params.id,
+                }).then((deletedDoc) => {
+                    if (deletedDoc.deletedCount > 0) {
+                       // console.log(user.created_list.filter(e => e.toString() === req.params.id))
+                        user.created_list = user.created_list.filter(e => e.toString() != req.params.id)
+                        user.save()
+                        MovieLists.find().then((data) => {
+                            res.status(200).json({ result: true, token: data.id });
+                        })
+                    } else {
+                        res.status(400).json({ result: false, error: "MovieList not found" });
+                    }
+                }
+                )
+            } else {
+                res.status(500).json({ success: false, message: "Erreur serveur", error: error.message });
+            }
+        });
     } catch (error) {
         res.status(500).json({ success: false, message: "Erreur serveur", error: error.message });
     }
-})
 
 
+});
 
-
-
-
-
-
-//??
 router.get('/get/:token/:id', async (req, res) => {
 
     try {
@@ -522,6 +323,8 @@ router.get('/get/:token/:id', async (req, res) => {
         console.error('Error occurred:', error);
         res.status(500).json({ success: false, message: error.message });
     }
+
+
 
 })
 
